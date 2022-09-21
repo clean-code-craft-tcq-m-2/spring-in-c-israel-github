bc56c94d37d5e7a434d53209b44b13cc4dd4a7be
Arsiel <israhell_oz@hotmail.com>
2022-09-21 12:49:03 -0500

    Adjusted changes
    Added typedef and additional REQUIRE statements

--- stats-test.cpp
+++ stats-test.cpp
@@ -9,7 +9,7 @@
 TEST_CASE("reports average, minimum and maximum") {
     float numberset[] = {1.5, 8.9, 3.2, 4.5};
     int setlength = sizeof(numberset) / sizeof(numberset[0]);
-    struct Stats computedStats = compute_statistics(numberset, setlength);
+    Stats_t computedStats = compute_statistics(numberset, setlength);
     float epsilon = 0.001;
     REQUIRE(abs(computedStats.average - 4.525) < epsilon);
     REQUIRE(abs(computedStats.max - 8.9) < epsilon);
@@ -23,6 +23,9 @@ TEST_CASE("average is NaN for empty array") {
     
     //Design the REQUIRE statement here.
     //Use https://stackoverflow.com/questions/1923837/how-to-use-nan-and-inf-in-c
+    REQUIRE(computedStats.average == NAN);
+    REQUIRE(computedStats.max == NAN);
+    REQUIRE(computedStats.min == NAN);
 }
 
 TEST_CASE("raises alerts when max is greater than threshold") {
@@ -32,7 +35,7 @@ TEST_CASE("raises alerts when max is greater than threshold") {
 
     float numberset[] = {99.8, 34.2, 4.5};
     int setlength = sizeof(numberset) / sizeof(numberset[0]);
-    Stats computedStats = compute_statistics(numberset, setlength);
+    Stats_t computedStats = compute_statistics(numberset, setlength);
 
     const float maxThreshold = 10.2;
     check_and_alert(maxThreshold, alerters, computedStats);
