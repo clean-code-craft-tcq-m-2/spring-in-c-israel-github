6b2a811b0cd0ff8ee6ba064c52a17d2097062746
Arsiel <israhell_oz@hotmail.com>
2022-09-21 12:39:09 -0500

    Added declarations and includes

--- stats.h
+++ stats.h
@@ -1,8 +1,19 @@
+#include "alerters.h"
 
-struct Stats compute_statistics(const float* numberset, int setlength);
+typedef struct Stats_s
+{
+	float average;
+	float min;
+	float max;
+} Stats_t;
 
 typedef void (*alerter_funcptr)();
-void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
+
+Stats_t compute_statistics(const float* numberset, int setlength);
+
+alerter_funcptr alerters[] = {emailAlerter, ledAlerter};
+
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], Stats_t computedStats);
 
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
